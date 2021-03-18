cafbd8553e3e02b962593206c53772e2096f1892
Dmitrii Zozulin <dmitriizozulin@gmail.com>
2021-03-18 03:41:53 +0300

    Fixes for Pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './components/App';
 
 const router = new createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/koshelek_test-task/'),
   routes: [
     { path: '/', component: () => import('./components/Table'), props: true },
     { path: '/diff', component: () => import('./components/Diff'), props: true },
